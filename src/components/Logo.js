ed786a0cae709e7e74c370eec6c735a92b0214e9
Abhishek Samariya <asamariya@gmail.com>
2019-11-29 00:09:10 +1100

    Added blue waves to logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Logo = () => {
   return (
-    <div className="text-center mb-4">
-      <h1>AugusTube</h1>
+    <div className="text-center logo mb-4">
+      <a href="/">
+        <span className="relative highlight highlight-aqua">
+          <h1 className="relative z-2"> AugusTube</h1>
+        </span>
+      </a>
     </div>
   );
 };
